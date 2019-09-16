0946a868e95ab15e85d6cb885bcaa97bd2fd96f6
Daniel Ashcroft <dashcroft4@gmail.com>
2019-09-16 13:02:09 +0100

    Added videos file and starting func to view name of first video.
    The video file should end up holding all the videos to transfer. I want to be able to just read and sent the name a file before trying to do it with an entire list.

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+)
 
-func main()  {
-	fmt.Println("hello World")
+/*
+HelloWorld to test if things are alright
+ */
+func sayHello(w http.ResponseWriter, r *http.Request) {
+
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	_, _ = w.Write([]byte(message))
+}
+
+/*
+Get the name of the first video.
+ */
+func sayNameOfFirstVideo(w http.ResponseWriter, r *http.Request)  {
+	message := "asd"
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/getFirstVideoName", sayNameOfFirstVideo)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
