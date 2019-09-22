17428ac64ef4b960056646e7dd207ff69b3e1651
Daniel Ashcroft <dashcroft4@gmail.com>
2019-09-22 12:31:55 +0100

    trying to get figure out how to respond http request with a list of details of the videos.

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 )
+
+const videoFilePath string = "videos"
 
 /*
 HelloWorld to test if things are alright
@@ -16,17 +21,75 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte(message))
 }
 
+func sayNamesOfVideos(w http.ResponseWriter, r *http.Request)  {
+
+	//opens file
+	f, err := os.Open(videoFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	videoFilesInfo, err := f.Readdir(-1)
+
+	//closes file
+	_ = f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+
+	println("videoFilesInfo", videoFilesInfo)
+	fmt.Printf("type:%T    size: %v\n", videoFilesInfo, len(videoFilesInfo))
+	
+	
+	
+
+	//sends the list of names
+	for _, file := range videoFilesInfo {
+		fmt.Println(file.Name())
+		_, _ = w.Write([]byte(file.Name()))
+	}
+
+}
+
 /*
 Get the name of the first video.
  */
 func sayNameOfFirstVideo(w http.ResponseWriter, r *http.Request)  {
-	message := "asd"
-	w.Write([]byte(message))
+	//message := "asd"
+
+	//opens file
+	f, err := os.Open(videoFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+
+	//closes file
+	_ = f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//sends the list of names
+	for _, file := range files {
+		fmt.Println(file.Name())
+		_, _ = w.Write([]byte(file.Name()))
+	}
+
+	//_, _ = w.Write([]byte(message))
 }
+
+
 
 func main() {
 	http.HandleFunc("/", sayHello)
 	http.HandleFunc("/getFirstVideoName", sayNameOfFirstVideo)
+	http.HandleFunc("/getVideoFileNames", sayNamesOfVideos)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
